525b4f18c3a676c252193926364d6da92bd71c00
Madhur <sharma15madhur@gmail.com>
2024-06-30 17:42:04 +0530

    updated portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,35 +1,20 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import weatherApp from "../assets/portfolio/weatherApp.jpg";
+import spaceScout from "../assets/portfolio/spaceScout.jpg";
+
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: spaceScout,
+      demo: "https://mellifluous-banoffee-47877b.netlify.app/",
+      code: "https://github.com/Madhur-Bakshi/mySpaceApp",
     },
     {
       id: 2,
-      src: reactParallax,
-    },
-    {
-      id: 3,
-      src: navbar,
-    },
-    {
-      id: 4,
-      src: reactSmooth,
-    },
-    {
-      id: 5,
-      src: installNode,
-    },
-    {
-      id: 6,
-      src: reactWeather,
+      src: weatherApp,
+      demo: "https://admirable-marigold-fefbe7.netlify.app/",
+      code: "https://github.com/Madhur-Bakshi/Weather-app-using-HTML-CSS-and-JAVASCRIPT",
     },
   ];
 
@@ -46,21 +31,27 @@ const Portfolio = () => {
           <p className="py-6">Check out some of my work right here</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+        <div className="grid sm:grid-cols-2 gap-12 px-12 sm:px-0 ">
+          {portfolios.map(({ id, src, demo, code}) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-105"
+                className="rounded-md duration-200 hover:scale-105 h-80 w-full"
               />
-              <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+              <div className="flex items-center justify-center bg-white rounded-b-lg">
+                <a href = {demo} target = "_blank">
+
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-xl">
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                </a>
+                <a href = {code} target="_blank">
+
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-xl">
                   Code
                 </button>
+                </a>
               </div>
             </div>
           ))}
